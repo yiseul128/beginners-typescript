e05380cda2065d39fcf851f27485bc71c8c7d867
Yiseul Ko <gls20020128@gmail.com>
2022-11-12 23:04:29 -0500

    Solve 02

--- src/02-object-param.problem.ts
+++ src/02-object-param.problem.ts
@@ -1,6 +1,11 @@
 import { expect, it } from "vitest";
 
-export const addTwoNumbers = (params) => {
+type Param = {
+  first: number,
+  second: number
+}
+
+export const addTwoNumbers = (params : Param) => {
   return params.first + params.second;
 };
 
